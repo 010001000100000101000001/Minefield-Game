74a3f716c009239ec56494503912d4ebb0ab7e21
David Anderson <davidanderson@live.ie>
2024-02-06 13:01:16 +0000

    Implement Board class constructor for inital board setup

--- run.py
+++ run.py
@@ -8,7 +8,12 @@ NUM_MINES = 10
 
 class Board:
     def __init__(self, size):
-        self.size = size
+        self.size = size # Size of the game board
+        self.board = [["." for _ in range (size)] for _ in range(size)] #Initialize the board with empty spaces
+        self.mines = [] # List to store mines position
+        self.start = (0, 0) # Start top-left corner
+        self goal = (size -1, size -1) # Finish bottom-right corner
+
 
 #Main functions 
 def main():
