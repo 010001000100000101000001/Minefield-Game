9250128e9ebbef8ed35d1ad1d9033933d4f608dc
David Anderson <davidanderson@live.ie>
2024-02-06 12:49:31 +0000

    Implement Board class constructor with board initialization

--- run.py
+++ run.py
@@ -6,4 +6,15 @@ import sys
 BOARD_SIZE = 10
 NUM_MINES = 10
 
+class Board:
+    def __init__(self, size):
+        self.size = size
+
+#Main functions 
+def main():
+
+
+if __name__ == "__main__"
+    main()
+
 
